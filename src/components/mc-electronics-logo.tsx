c7d1132803429844ac685ac5fac47cfeaa639f2b
Sandeep M <723msfree@gmail.com>
2025-08-18 13:32:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/mc-electronics-logo.tsx
@@ -0,0 +1,12 @@
+
+export const MCElectronicsLogo = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-primary" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M4 11a8 8 0 0 1 8 8" />
+        <path d="M4 4a15 15 0 0 1 15 15" />
+        <path d="M4 18a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-3" />
+        <path d="M12 11v4" />
+        <path d="m14 13-2 2-2-2" />
+        <path d="M20 4h-4" />
+        <path d="M18 6V2" />
+    </svg>
+  );
